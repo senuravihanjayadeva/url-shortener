9462b4b0e1bc9ee2978b64917e2973cd0cde86ce
SenuraJayadeva <53615169+SenuraJayadeva@users.noreply.github.com>
2020-06-23 00:54:13 +0530

    final

--- server.js
+++ server.js
@@ -9,13 +9,25 @@ mongoose.connect("mongodb://localhost/urlShortener", {
 });
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
 });
 
 app.post("/shortUrls", async (req, res) => {
-  ShortUrl.create({ full: req.body.fullUrl });
+  await ShortUrl.create({ full: req.body.fullUrl });
   res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
 });
 
 app.listen(process.env.PORT || 5000);
