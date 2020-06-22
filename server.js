1509d2359991a6f53b26a4c1c2cdde4fa71a3194
SenuraJayadeva <53615169+SenuraJayadeva@users.noreply.github.com>
2020-06-23 00:38:09 +0530

    implemented model

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
 const app = express();
+
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.post("/shortUrls", async (req, res) => {
+  ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 5000);
